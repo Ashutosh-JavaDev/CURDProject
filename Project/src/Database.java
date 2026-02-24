b4ee7f61de8aea8f3404e6cdf31ce831b38f6c28
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-24 17:19:14 +0530

    Java DataBase Connectivity

--- Project/src/Database.java
+++ Project/src/Database.java
@@ -1,5 +1,11 @@
 public class Database {
+    private final  String url="";
+    private final  String username="";
+    private final  String password="";
+    Database(){
+       
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      
     }
 }
