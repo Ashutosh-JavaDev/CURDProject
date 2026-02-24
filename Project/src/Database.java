d5725eeafc8375dfd8162e792598083e0fcb5bf2
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-24 17:26:19 +0530

    Java DataBase Connectivity

--- Project/src/Database.java
+++ Project/src/Database.java
@@ -1,9 +1,14 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Driver;
+import java.sql.DriverManager;
 public class Database {
-    private final  String url="";
-    private final  String username="";
-    private final  String password="";
+    private final  String url="jdbc:mysql://localhost:3306/?JDBC_Project";
+    private final  String username="root";
+    private final  String password="@Radhakrishna297";
     Database(){
-       int a;
+       
     }
     public static void main(String[] args) throws Exception {
       
